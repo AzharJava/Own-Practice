a9d9b7696b26b1877b9d15bbc8af9d39b0c1a4c2
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-04 22:30:05 +0530

    example of instance variable
    OUTPUT
    Marks for first object:
    engMarks: 50
    mathsMarks: 80
    phyMarks: 90
    Marks for second object:
    engMarks: 80
    mathsMarks: 60
    phyMarks: 85

---
+++ instanceVaribable.java
@@ -0,0 +1,38 @@
+
+class Marks {
+	// These variables are instance variables.
+	// These variables are in a class
+	// and are not inside any function
+	int engMarks;
+	int mathsMarks;
+	int phyMarks;
+}
+
+class MarksDemo {
+	public static void main(String args[])
+	{
+		// first object
+		Marks obj1 = new Marks();
+		obj1.engMarks = 50;
+		obj1.mathsMarks = 80;
+		obj1.phyMarks = 90;
+
+		// second object
+		Marks obj2 = new Marks();
+		obj2.engMarks = 80;
+		obj2.mathsMarks = 60;
+		obj2.phyMarks = 85;
+
+		// displaying marks for first object
+		System.out.println("Marks for first object:");
+		System.out.println("engMarks: "+obj1.engMarks);
+		System.out.println("mathsMarks: "+obj1.mathsMarks);
+		System.out.println("phyMarks: "+obj1.phyMarks);
+
+		// displaying marks for second object
+		System.out.println("Marks for second object:");
+		System.out.println("engMarks: "+obj2.engMarks);
+		System.out.println("mathsMarks: "+obj2.mathsMarks);
+		System.out.println("phyMarks: "+obj2.phyMarks);
+	}
+}

694c540e24e73c83444cd2045d2e00c7b470a4e7
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-04 12:34:57 +0530

    Example for constructor
    Output
    100 100

---
+++ consDemo.java
@@ -0,0 +1,12 @@
+class MyClass{
+	int num;
+	MyClass(){
+		num=100;
+	}
+
+	public static void main(String[] args) {
+	MyClass t1=new MyClass();
+	MyClass t2=new MyClass();
+	System.out.println(t1.num+" "+t2.num);
+	}
+}

4c2493485fd8a51b58fe6e74bde57b39699692c7
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-04 22:06:55 +0530

    Example of data type

---
+++ dataType.java
@@ -0,0 +1,33 @@
+package dataType;
+
+public class dataType {
+	public static void main(String args[]) {
+		//declear character
+		char a='A';
+		
+		//declear integer
+		int i = 20;
+		
+		//declear byte
+		byte b=4;
+		
+		//deaclear sort
+		short s=56;
+		
+		//declear double
+		double d= 4.8764875786;
+		
+		//float decalear
+		float f=4.867527f;
+		
+		//print all the data type
+		System.out.println("Char: "+a);
+		System.out.println("Integer: "+i);
+		System.out.println("byte : "+b);
+		System.out.println("short: "+s);
+		System.out.println("double: "+d);
+		System.out.println("float: "+f);
+		
+	}
+
+}

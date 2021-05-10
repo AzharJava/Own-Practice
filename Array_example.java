0404980a5fa2cdbf4700a811717d2c51b58e9b0c
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-05-10 14:21:36 +0530

    Array.java
    OUTPUT :
    1
    6
    5
    4
    6

---
+++ Array_example.java
@@ -0,0 +1,10 @@
+
+
+public class App {
+    public static void main(String[] args)  {
+        int [] myArray={1, 6, 5, 4, 6};
+        for(int i=0; i<myArray.length; i++){
+            System.out.println("Array is: "+myArray[i]);
+        }
+    }
+}
